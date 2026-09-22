04f7638122c60e014a6aff953465550d408c4bb9
Etna2886 <etnag@Etna_HP>
2026-09-22 12:56:08 +0200

    refactor main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package main;
 
@@ -10,12 +9,7 @@ package main;
  * @author etnag
  */
 public class Main {
-
-    /** 
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         // TODO code application logic here
     } 
-    
 }
